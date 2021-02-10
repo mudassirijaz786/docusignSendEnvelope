2254e43d273c1245d521c1e32d191a4824008cd2
Mudassir Ijaz <mudassir.ijaz@kwanso.com>
2021-02-10 12:49:10 +0500

    editing in details json

--- envelopeCreation.js
+++ envelopeCreation.js
@@ -8,6 +8,41 @@ function makeEnvelope(envelopeArgs) {
 
   let [templateRoles] = envelopeArgs.templateRoles;
 
+  let { tabs } = templateRoles;
+  let [dateSignedTabs] = tabs.dateSignedTabs;
+  let [
+    company,
+    title,
+    address,
+    city,
+    fullName,
+    spellPercentage,
+    percentage,
+    spellDays,
+    voilationDays,
+    notifyDays,
+    terminationDays,
+    companyAgain,
+  ] = tabs.textTabs;
+
+  const c = "Lahore";
+  const s = "Punjab";
+  const z = "54000";
+
+  dateSignedTabs.value = new Date();
+  company.value = "Kwanso";
+  title.value = "Nadeem Yasin";
+  address.value = "353-A, Johar Town";
+  fullName.value = "Developer";
+  city.value = `${c}, ${s} ${z}`;
+  spellPercentage.value = "Fifteen Percent";
+  percentage.value = "15%";
+  spellDays.value = "thirty days";
+  voilationDays.value = "30";
+  notifyDays.value = "30";
+  terminationDays.value = "30";
+  companyAgain.value = "Kwanso";
+
   envelope.templateRoles = [templateRoles];
 
   envelope.status = envelopeArgs.status;
